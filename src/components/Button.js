3cd1fb5947b28785abebab7f26664ec88a72e3c5
Lena <lena.boruslav@gmail.com>
2019-07-05 15:44:49 +0300

    Add missed files

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Button, Container, Header } from 'semantic-ui-react'
+
+//const ButtonMain = document.createElement("link");
+
+const ButtonMain = () => { }
+//styleLink.rel = "stylesheet";
+//styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+//document.head.appendChild(styleLink);
+
+
+
+export default ButtonMain
